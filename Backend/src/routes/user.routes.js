74b948ca536a44e6c29027f192221c0f7e77b3b6
David <davidrsmk144@gmail.com>
2022-12-11 13:14:34 -0500

    Test production

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -4,5 +4,8 @@ const UserController = require('../controllers/userController')
 const { verifyToken, verifyId } = require('../middlewares/authjwt')
 
 router.post('/getUser', [ verifyToken, verifyId ], UserController.getUserById)
+router.get('/test', (req, res) => {
+    res.json('hola')
+})
 
 module.exports = router
